2925c436b7caae52acb89efe2be24419cb12876a
Agus Safarudin <42315161+agussafarudin@users.noreply.github.com>
2019-01-05 22:06:11 +0700

    safarudin

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<conio.h>
+#include<stdio.h>
+
+void secara_nilai(float a, float b, char c){
+float * Alamat_A;
+Alamat_A= &a;
+a=?;
+printf("Lokal a=%f, alamat A = %p\n ", a, Alamat_A);
+printf("Lokal b =%f\n",b);
+printf("Lokal c=%f\n",c);
+}
+int main(){
+float a=25, * Alamat_A;
+char c=
+}
